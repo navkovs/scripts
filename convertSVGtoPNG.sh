3391beb06cf348b1989cce1f8a4c3c5178b30074
navkovs <jjws@gmx.de>
2023-10-26 11:31:19 +0200

    Removed duplicates

--- convertSVGtoPNG.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Convert all files in a dir from SVG to PNG.
-# Preserve the original files.
-
-for file in *.svg
-do
-     /usr/bin/rsvg-convert "${file}" >  "${file}.png"
-done
--- micToggle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Toggle Microphone on and off. Set this script to a hotkey.
-
-amixer set Capture toggle
